e4d9ac7fb37ba5149d6d452aff01138a67b5bcb6
Wei Jiang <wei@WJiang.local>
2019-02-25 23:09:19 -0500

    feb 26

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
-var listofwords = ["thor", "spiderman", "hulk", "thanos", "antman", 
-"deadpool","wolverine", "juggernaut", "quicksilver", "cable", "magneto",
-"gamora","storm", "mystique"];
-var randomNumber= Math.floor(Math.random() * listofwords.length);
+var listofwords = ["thor", "spiderman", "hulk", "thanos", "antman",
+    "deadpool", "wolverine", "juggernaut", "quicksilver", "magneto",
+    "gamora", "mystique"];
+
+
+var randomNumber = Math.floor(Math.random() * listofwords.length);
 var pickedWord = listofwords[randomNumber];
 var rightWord = [];
 var wrongLetter = [];
 var underscore = [];
-var userLetter =[];
+var userLetter = [];
 var guessCount = 10;
 var win = [];
 var lose = [];
@@ -14,41 +16,46 @@ var lose = [];
 console.log(pickedWord);
 
 var HtmlUnderScore = document.getElementById("underscores");
-// var HtmlRightWord = document.getElementsByClassName("rightGuess");
 var HtmlwrongLetter = document.getElementById("wrongGuess");
 var HtmlCountDown = document.getElementById("attempts");
 var HtmlWin = document.getElementById("win");
 
 var generateUnderscore = () => {
-    for (var i = 0; i < pickedWord.length; i++){
+    for (var i = 0; i < pickedWord.length; i++) {
         underscore.push("_");
     }
     return underscore;
 }
 console.log(generateUnderscore());
 
+
+
 document.addEventListener("keypress", (event) => {
     var userLetter = String.fromCharCode(event.keyCode);
-    for (var x = 0; x < pickedWord.length; x++){
-    if (pickedWord[x] === userLetter){
-        console.log(userLetter);
-        underscore[x] = userLetter;
-        HtmlUnderScore.innerHTML = underscore.join(" ");
+    var sameLetter = false;
+    for (var x = 0; x < pickedWord.length; x++) {
+        if (pickedWord[x] === userLetter) {
+            console.log(userLetter);
+            underscore[x] = userLetter;
+            HtmlUnderScore.textContent = underscore.join(" ");
+        }
     }
-    // console.log(pickedWord.indexOf(userLetter))
-    if (pickedWord.indexOf(userLetter) > -1 ) {
-
+    if (pickedWord.indexOf(userLetter) > -1) {
         rightWord.push(userLetter);
         underscore[pickedWord.indexOf(userLetter)] = userLetter;
         HtmlUnderScore.innerHTML = underscore.join(" ");
-        if (underscore.join("") == pickedWord){
+        if (underscore.join("") == pickedWord) {
             alert("You Win");
         }
         console.log(underscore);
     }
-    else { 
+    else {
+        if (sameLetter === false) {
+            if (wrongLetter.includes(userLetter)) {
+                alert("You Already Guess This Letter");
+            }
+        }
         wrongLetter.push(userLetter);
-
         console.log(wrongLetter);
         HtmlCountDown.innerHTML = guessCount;
         HtmlwrongLetter.innerHTML = wrongLetter.join(" ");
@@ -57,12 +64,20 @@ document.addEventListener("keypress", (event) => {
         guessCount--;
         if (guessCount === 0) {
             alert("Game Over")
-            return;        
+            return;
         }
-    // }
-    }}
+    }
+
+
 });
 
+// var sameLetter = function() {
+//     var sameletter = false;
+//     for (var t = 0; t < pickedWord.length; t++){
+//         if (pickedWord[t]
+//     }
+
+// }
 
 
 
